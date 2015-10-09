e2740d6166f9ba0dc61c576520f5ebc0d2f4f497
Fakirappa <fakeerappa.b@gmail.com>
2015-10-09 12:35:53 +0530

    commit changes

--- clone.py
+++ clone.py
@@ -12,6 +12,8 @@ def run_command(cmd):
 def main():
     # print command line arguments
     run_command('ssh -l fmelsak brmpen-cs12-0.us.oracle.com')
+    umask 022
+    run_command('export PATH=/ws/on12-tools/onbld/bin/:/ws/onnv-tools/teamware/bin:/opt/onbld/bin/`uname -p`:/usr/bin:/usr/sbin')
     for arg in sys.argv[1:]:
         print arg
 
